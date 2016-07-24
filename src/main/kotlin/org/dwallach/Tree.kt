d63c843ec1c08283734e9abd4660062cc7cfb096
Dan Wallach <dwallach@cs.rice.edu>
2016-07-24 10:21:05 -0500

    attempt to use the new typealias feature

--- src/main/kotlin/org/dwallach/Tree.kt
+++ src/main/kotlin/org/dwallach/Tree.kt
@@ -28,8 +28,13 @@ interface Tree<T: Comparable<T>> {
 
     fun value(): T
 
-    fun <R> match(emptyFunc: () -> R, nonEmptyFunc: (value: T, left: Tree<T>, right: Tree<T>) -> R): R =
-      if(empty()) emptyFunc() else nonEmptyFunc(value(), left(), right())
+    typealias nonEmptyFuncAlias<T,R> = (T, Tree<T>, Tree<T>) -> R
+
+    /**
+     * Structural pattern matching on a tree: calls one or the other lambda depending on whether the
+     * tree node in question is a leaf or an internal node.
+     */
+    fun <R> match(emptyFunc: () -> R, nonEmptyFunc: nonEmptyFuncAlias<T,R>): R
 
     fun size(): Int = match({ 0 }, { value, left, right -> left.size() + right.size() + 1 })
 
@@ -59,6 +64,8 @@ interface Tree<T: Comparable<T>> {
     }
 
     private data class NonEmptyTree<T: Comparable<T>>(val nodeValue: T, val treeLeft: Tree<T>, val treeRight: Tree<T>): Tree<T> {
+        override fun <R> match(emptyFunc: () -> R, nonEmptyFunc: nonEmptyFuncAlias<T,R>): R = nonEmptyFunc(nodeValue, treeLeft, treeRight)
+
         override fun empty() = false
 
         override fun insert(newbie: T): Tree<T> {
@@ -78,6 +85,12 @@ interface Tree<T: Comparable<T>> {
     }
 
     private object emptyTreeSingleton: Tree<Comparable<Any>> {
+        // this works
+        override fun <R> match(emptyFunc: () -> R, nonEmptyFunc: (Comparable<Any>, Tree<Comparable<Any>>, Tree<Comparable<Any>>) -> R): R = emptyFunc()
+
+        // this says that nonEmptyFuncAlias requires three type parameters, which is incorrect
+//        override fun <R> match(emptyFunc: () -> R, nonEmptyFunc: nonEmptyFuncAlias<Comparable<Any>, R>): R = emptyFunc()
+
         override fun insert(newbie: Comparable<Any>): Tree<Comparable<Any>> = NonEmptyTree(newbie, this, this)
 
         override fun left(): Tree<Comparable<Any>> {
